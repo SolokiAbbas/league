294142e96424c62c5925dfc1a5c010770e524bb4
Abbas <SolokiAbbas@gmail.com>
2018-02-04 00:07:55 -0500

    added navbar comp

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,1 +1,18 @@
 import React from 'react';
+
+class NavBar extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd; height: 40px;" >
+          <a class="navbar-brand" href="#">Home</a>
+          <a class="navbar-brand" href="#">About</a>
+          <a class="navbar-brand" href="#">Link</a>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
