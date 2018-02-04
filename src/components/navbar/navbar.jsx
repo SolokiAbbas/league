9757280005db8f7d16c835e20cc8af900a2cbbc5
Abbas <SolokiAbbas@gmail.com>
2018-02-04 18:51:56 -0500

    fixed navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd; height: 40px;" >
+        <nav className="navbar navbar-expand-lg navbar-light nav-custom">
           <a className="navbar-brand" href="#">Home</a>
           <a className="navbar-brand" href="#">About</a>
           <a className="navbar-brand" href="#">Link</a>
