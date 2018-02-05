3aca31f2d037d58e96d4406051d9b00252964b4b
Abbas <SolokiAbbas@gmail.com>
2018-02-05 14:05:41 -0500

    added about page

---
+++ src/components/navbar/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => (
+      <div>
+        <p>I am experienced in Ruby on Rails and JavaScript.
+          Currently, I am working on solo Project using Python and Django.
+          Solving difficult algorithms is my hobby.
+          I look forward to experience new technologies and challenges.
+        </p>
+      </div>
+    );
+
+export default About;
