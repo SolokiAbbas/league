4f1b8bae9284ea4eaa8370a79db47f361ddb464d
Abbas <SolokiAbbas@gmail.com>
2018-02-05 14:03:52 -0500

    added links in header nav

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import NavBar from './navbar/navbar';
+import About from './navbar/about';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
   render(){
     return(
       <div>
-        <NavBar />
+        <Route path="/" component={ NavBar } />
+        <Route path="/about" component={ About } />
       </div>
     );
   }
