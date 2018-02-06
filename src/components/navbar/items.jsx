229ef3047bb592c9943d106168dcecc7598f69f7
Abbas <SolokiAbbas@gmail.com>
2018-02-06 10:00:09 -0500

    added files

---
+++ src/components/navbar/items.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Items = () => (
+      <div className="jumbotron mx-auto jumbo-about">
+        <p>I am experienced in Ruby on Rails and JavaScript.
+          Currently, I am working on a solo Project using Python and Django.
+          Solving difficult algorithms is my hobby.
+          I look forward to experience new technologies and challenges.
+        </p>
+      </div>
+    );
+
+export default Items;
