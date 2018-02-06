10c12aa7f1839e9e42e90868241d45f94bcdcb1e
Abbas <SolokiAbbas@gmail.com>
2018-02-06 09:57:51 -0500

    added new navbar lists

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,6 +6,10 @@ const NavBar = () => (
         <nav className="navbar navbar-expand-lg navbar-light nav-custom">
           <Link className="navbar-brand" to="/">Home</Link>
           <Link className="navbar-brand" to="/about">About</Link>
+          <Link className="navbar-brand" to="/about">Champions</Link>
+          <Link className="navbar-brand" to="/about">Search Summoner Name</Link>
+          <Link className="navbar-brand" to="/about">Summoner Spells</Link>
+          <Link className="navbar-brand" to="/about">Summoner Items</Link>
         </nav>
       </div>
     );
