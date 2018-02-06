1e0625a13ff0b2dfeb6385663bf972e82fc5258a
Abbas <SolokiAbbas@gmail.com>
2018-02-06 10:05:29 -0500

    added routes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import NavBar from './navbar/navbar';
 import About from './navbar/about';
+import Champions from './navbar/champions';
+import Items from './navbar/items';
+import Spells from './navbar/spells';
+import Summoners from './navbar/summoners';
 import { Route } from 'react-router-dom';
 
 class App extends React.Component {
@@ -9,6 +13,10 @@ class App extends React.Component {
       <div>
         <Route path="/" component={ NavBar } />
         <Route path="/about" component={ About } />
+        <Route path="/champions" component={ Champions } />
+        <Route path="/items" component={ Items } />
+        <Route path="/spells" component={ Spells } />
+        <Route path="/summoners" component={ Summoners } />
       </div>
     );
   }
