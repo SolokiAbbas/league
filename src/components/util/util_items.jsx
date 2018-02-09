dc662df48fa39e40bfb0834b59ae5d7fb36d7c47
Abbas <SolokiAbbas@gmail.com>
2018-02-08 19:21:35 -0500

    api fixed

--- src/components/util/util_items.jsx
+++ src/components/util/util_items.jsx
@@ -1,10 +1,5 @@
 export const fetchItemsAPI = (url) => {
   fetch(url).then((res) => {
           return res.json();
-        }).then(data=>{
-          return({
-            champion: data.id,
-            name:data.name
-          });
         });
 };
