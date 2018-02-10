1ab1e7fac866d17f3d09983c42e29ca0019409bd
Abbas <SolokiAbbas@gmail.com>
2018-02-09 22:56:30 -0500

    added json file

--- src/components/data/items.jsx
+++ src/components/data/items.jsx
@@ -1,30 +1,49 @@
 import React from 'react';
-import fetchItems from '../util/util_items';
+// import fetchItems from '../util/util_items';
 
 class Items extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      itemList: [],
+      itemsList: {},
     };
-    this.fetchItems = this.fetchItems.bind(this);
+    this.data = {};
+    this.fetchItemsList = this.fetchItemsList.bind(this);
   }
-  fetchItems(){
-    const url ="https://na1.api.riotgames.com/lol/static-data/v3/items?locale=en_US&api_key=RGAPI-149ba0a2-62de-46e0-8c23-9ae7f2930aa1";
-    fetchItems().then(data=>{
-      this.setState({data});
+  componentDidMount(){
+    this.fetchItemsList();
+  }
+
+  fetchItemsList(){
+    // const url ="https://na1.api.riotgames.com/lol/static-data/v3/items?locale=en_US&api_key=RGAPI-149ba0a2-62de-46e0-8c23-9ae7f2930aa1";
+    // fetchItems().then(data=>{
+    //   this.setState({data});
+    // });
+    // data=> this.setState({datas: data})
+    let items = {};
+    $.getJSON("../../data/items.json", function (data) {
+      $.each(data, function (index, value) {
+          items[index]=value;
+        });
+        // console.log(items.data);
     });
+    this.setState({itemsList : items.data});
+    console.log(items.data);
   }
 
   render(){
-    return(
-          <div className="jumbotron mx-auto jumbo-about">
-            {this.fetchItems()}
-            {console.log(this.state)}
-            <p>{this.state.itemList}</p>
-          </div>
-        );
-  }
+    if(typeof this.state !== 'undefined'){
+      return(
+        <div className="jumbotron mx-auto jumbo-about">
+
+          // {console.log(this.state.itemsList)}
+          <p></p>
+        </div>
+      );
+     }else{
+     return(<div></div>);
+    }
+   }
 }
 
 export default Items;
--- src/components/util/util_champ.jsx
+++ src/components/util/util_champ.jsx
@@ -1,3 +1,12 @@
 export const fetchChampsAPI = (url, app) => {
-
+  fetch(url,{method:'GET', headers: {
+    "Origin": "http://localhost:8080",
+    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+    "X-Riot-Token": "RGAPI-93111a49-da86-4648-99e0-84ae8b063dae",
+    "Accept-Language": "en-US,en;q=0.9",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
+  }
+}).then((res) => {
+          return res.json();
+        });
 };
