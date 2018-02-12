a2f136ef6ddfc3c16e09bbbdc69418cff49299a1
Abbas <SolokiAbbas@gmail.com>
2018-02-11 21:18:33 -0500

    added items

--- src/components/items/item_detail.jsx
+++ src/components/items/item_detail.jsx
@@ -6,6 +6,7 @@ class ItemDetail extends React.Component{
   render(){
     return(
       <div>
+        <h1>Hello</h1>
         <h3>{this.props.name}</h3>
         <h3>{this.props.detail}</h3>
       </div>
--- src/components/items/items.jsx
+++ src/components/items/items.jsx
@@ -32,11 +32,10 @@ class Items extends React.Component{
 
     if(typeof this.state.itemsList[1001] !== 'undefined'){
       const allItems = Object.keys(this.state.itemsList).map(el=> this.state.itemsList[el]);
-      console.log(allItems);
-      console.log(this.props);
+
       return(
         <div className="jumbotron mx-auto jumbo-about">
-          {allItems.map((idx,item)=> <ItemDetail detail={allItems[idx.toString()].plaintext} name={allItems[idx.toString()].name}/>)}
+          {allItems.map(item => <ItemDetail key={item.id} detail={item.plaintext} name={item.name}/>)}
         </div>
       );
      }else{
