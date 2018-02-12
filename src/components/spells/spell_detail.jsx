bb0641b55bcfa3f937d8ff8f6ab73cdc46815b8c
Abbas <SolokiAbbas@gmail.com>
2018-02-12 18:48:54 -0500

    added spells and images

--- src/components/spells/spell_detail.jsx
+++ src/components/spells/spell_detail.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 
 
 class SpellDetail extends React.Component{
-
+  render(){
+    return(
+        <div className="card item-details" style={{width: '18rem'}}>
+          <img className="card-img-top" src={`https://ddragon.leagueoflegends.com/cdn/7.10.1/img/champion/${this.props.image.full}`} alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">{this.props.name}</h5>
+            <p className="card-text">{this.props.detail}</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default SpellDetail;
--- src/components/spells/spells.jsx
+++ src/components/spells/spells.jsx
@@ -28,11 +28,10 @@ class Spells extends React.Component{
   render(){
       if(typeof this.state.spellsList.SummonerBarrier !== 'undefined'){
         const allSpells = Object.keys(this.state.spellsList).map(el=> this.state.spellsList[el]);
-        console.log(allSpells);
         return(
           <div className="jumbotron mx-auto jumbo-about">
             <div className="items">
-              {allSpells.map(spell => <SpellDetail key={spell.id} detail={spell.description} name={spell.name} />)}
+              {allSpells.map(spell => <SpellDetail key={spell.id} detail={spell.description} name={spell.name} image={spell.image}/>)}
             </div>
           </div>
         );
