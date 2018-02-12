95008dfc4880c483bc81d369fbcd6760b338a4a6
Abbas <SolokiAbbas@gmail.com>
2018-02-12 18:15:21 -0500

    added card images

--- src/components/items/item_detail.jsx
+++ src/components/items/item_detail.jsx
@@ -5,7 +5,7 @@ class ItemDetail extends React.Component{
   render(){
     return(
       <div className="card" style={{width: '18rem'}}>
-        <img className="card-img-top" src="" alt="Card image cap" />
+        <img className="card-img-top" src="../../assets/images/default-image.png" alt="Card image cap" />
         <div className="card-body">
           <h5 className="card-title">{this.props.name}</h5>
           <p className="card-text">{this.props.detail}</p>
