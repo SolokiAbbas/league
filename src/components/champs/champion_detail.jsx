a5462111b1c0d9b38e98748f6f5a8fb2c44af828
Abbas <SolokiAbbas@gmail.com>
2018-02-13 09:19:35 -0500

    added champ details

--- src/components/champs/champion_detail.jsx
+++ src/components/champs/champion_detail.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 
 class ChampDetail extends React.Component{
-
+  render(){
+    return(
+        <div className="card item-details" style={{width: '20rem'}}>
+          <img className="card-img-top" src={`http://ddragon.leagueoflegends.com/cdn/8.3.1/img/item/${this.props.image.full}`} alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">{this.props.name}</h5>
+            <p className="card-text">Cost: {this.props.gold.base}</p>
+            <p className="card-text">Sells for: {this.props.gold.sell}</p>
+            <p className="card-text">{this.props.detail}</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default ChampDetail;
