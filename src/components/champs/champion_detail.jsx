c6db2faeac9e106bd4f4a9ec229147e330bce638
Abbas <SolokiAbbas@gmail.com>
2018-02-13 09:40:16 -0500

    added champs details

--- src/components/champs/champion_detail.jsx
+++ src/components/champs/champion_detail.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 class ChampDetail extends React.Component{
   render(){
+    let title = this.props.title.charAt(0).toUpperCase() + this.props.title.slice(1);
     return(
-        <div className="card item-details" style={{width: '20rem'}}>
-          <img className="card-img-top" src={`http://ddragon.leagueoflegends.com/cdn/8.3.1/img/champion/${this.props.image.full}`} alt="Card image cap" />
+        <div className="card item-details" style={{width: '16rem'}}>
+          <img className="card-img-top" style={{padding: '50px'}} src={`http://ddragon.leagueoflegends.com/cdn/8.3.1/img/champion/${this.props.image.full}`} alt="Card image cap" />
           <div className="card-body">
             <h5 className="card-title">{this.props.name}</h5>
-            <p className="card-text">"${this.props.title}"</p>
-            <p className="card-text">Sells for: {this.props.gold.sell}</p>
-            <p className="card-text">{this.props.detail}</p>
+            <p className="card-text">"{title}"</p>
+            <p className="card-text">{this.props.blurb}</p>
             <a href="#" className="btn btn-primary">Details!</a>
           </div>
         </div>
