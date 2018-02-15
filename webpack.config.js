ac05d0fd55fcdd2f4a1eb35bdad63b074fb5ec52
Abbas <SolokiAbbas@gmail.com>
2018-02-14 21:02:35 -0500

    reorganized files

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,100 @@
-const webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 var path = require("path");
 
 module.exports = {
-  context: __dirname,
-  entry: "./src/index.jsx",
+  entry: './src/js/index.jsx',
+
   output: {
-    path: path.join(__dirname, 'dist'),
-    sourceMapFilename: "./bundle.js.map",
-    filename: "bundle.js"
+    path: './dist',
+    filename: 'bundle.js'
   },
-  resolve: {
-    extensions: ["*", ".js", ".jsx"]
-  },
-  devtool: 'source-map',
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin(),
-  ],
+
   module: {
     loaders: [
-    {
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel-loader',
-      options: {
-        presets: ['es2015', 'react']
-      }
-    },
-    {
-      test: /\.node$/,
-      loader: "node-loader"
-    }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+            presets: ['es2015']
+        }
+      },
+      {
+        test: /\.less$/,
+        loader: "style!css!less"
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        include: /img/,
+        loader: 'url'
+      },
     ]
   },
-    devServer: {
-      inline: true,
-      contentBase: './dist',
-      port: 5000
-    }
+
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './src/index.html' }
+    ]),
+    new CopyWebpackPlugin([
+      { from: './src/vendors/phaser.min.js' }
+    ]),
+    new CopyWebpackPlugin([
+      { from: './src/assets', to: 'assets' }
+    ])
+  ],
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  devServer: {
+    inline: true,
+    contentBase: './dist',
+    port: 5000
+  }
 };
+
+
+
+//
+// const webpack = require('webpack');
+// var path = require("path");
+//
+// module.exports = {
+//   context: __dirname,
+//   entry: "./src/index.jsx",
+//   output: {
+//     path: path.join(__dirname, 'dist'),
+//     sourceMapFilename: "./bundle.js.map",
+//     filename: "bundle.js"
+//   },
+//   resolve: {
+//     extensions: ["*", ".js", ".jsx"]
+//   },
+//   devtool: 'source-map',
+//   plugins: [
+//     new webpack.optimize.UglifyJsPlugin(),
+//     new webpack.optimize.AggressiveMergingPlugin(),
+//   ],
+//   module: {
+//     loaders: [
+//     {
+//       test: /\.jsx?$/,
+//       exclude: /(node_modules|bower_components)/,
+//       loader: 'babel-loader',
+//       options: {
+//         presets: ['es2015', 'react']
+//       }
+//     },
+//     {
+//       test: /\.node$/,
+//       loader: "node-loader"
+//     }
+//     ]
+//   },
+//     devServer: {
+//       inline: true,
+//       contentBase: './dist',
+//       port: 5000
+//     }
+// };
