c0704223d24dbf2c8afcb60c28ab0aa1919f3882
Abbas <SolokiAbbas@gmail.com>
2018-02-18 14:49:11 -0500

    added search file

--- src/js/components/search/search.jsx
+++ src/js/components/search/search.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import SearchInput, {createFilter} from 'react-search-input'
+import SearchInput, {createFilter} from 'react-search-input';
 
-class Search extends React.Component{}
+class Search extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      searchTerm: ''
+    };
+    this.searchUpdate = this.searchUpdate.bind(this);
+  }
+}
 
 export default Search;
