1391b30bb18b1dc9cf5395161aba0eb458adc79a
Abbas <SolokiAbbas@gmail.com>
2018-02-17 22:13:45 -0500

    cleaned code

--- dist/bundle.js
+++ dist/bundle.js
@@ -23656,8 +23656,6 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-// import utilAPI from '../util/util_champ';
-
 var Champions = function (_React$Component) {
   _inherits(Champions, _React$Component);
 
