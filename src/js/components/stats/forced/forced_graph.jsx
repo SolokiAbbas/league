f7333a1a96b75e9dbcf22cbc0bac5c3ffcb8f483
Abbas <SolokiAbbas@gmail.com>
2018-02-23 02:15:59 -0500

    fixing json file

--- src/js/components/stats/forced/forced_graph.jsx
+++ src/js/components/stats/forced/forced_graph.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import * as d3 from 'd3';
+import { InteractiveForceGraph, ForceGraphNode, ForceGraphLink } from 'react-vis-force';
+
 
 class Forced extends React.Component{
-  constructor(props){
-    super(props);
-  }
-
-  componentDidMount(){
-  }
 
  render() {
    return (
-     <div className='jumobotron'>
-       <h1>National Contiguity</h1>
-       <div className='chartContainer'>
-         <svg className='chart'>
-         </svg>
-       </div>
-     </div>
+     <div></div>
+
    );
  }
 
--- src/js/components/stats/stats.jsx
+++ src/js/components/stats/stats.jsx
@@ -4,9 +4,44 @@ import Forced from './forced/forced_graph';
 class Stats extends React.Component{
   constructor(){
     super();
+    this.state = {
+      graph: {}
+    };
+    this.nodes = [];
+    this.queues = [];
+    this.allNodes = this.allNodes.bind(this);
+    this.allQueues = this.allQueues.bind(this);
+    this.fetchForced = this.fetchForced.bind(this);
+  }
+
+  componentWillMount(){
+    this.fetchForced();
+  }
+
+  fetchForced(){
+    let fgraph = {};
+      $.getJSON("../../../data/forced.json", function (data) {
+        console.log("here");
+        $.each(data, function (index, value) {
+            fgraph[index]=value;
+
+          });
+      }).fail((data,textStatus,err)=>{
+        let errors = textStatus + ", " + err;
+        console.log(errors);
+      }).then(datas => this.setState({graph : datas}));
+  }
+
+  allNodes(){
+    this.nodes = Object.keys(this.state.data).map(el=> this.state.data[el]);
+  }
+
+  allQueues(){
+    this.queues = Object.keys(this.state.data).map(el=> this.state.data[el]);
   }
 
   render(){
+    console.log(this.state);
     return(
       <div>
         <Forced></Forced>
