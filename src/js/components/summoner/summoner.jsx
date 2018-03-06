be9b8a1b8ef27a3961b1dc92632333cc201aa494
Abbas <SolokiAbbas@gmail.com>
2018-03-06 12:36:55 -0500

    fixed styling

--- src/js/components/summoner/summoner.jsx
+++ src/js/components/summoner/summoner.jsx
@@ -4,7 +4,9 @@ class Summoner extends React.Component{
   render(){
     return(
       <div className="card item-details text-center" style={{width: '15rem'}}>
-        <img className="sum-image" src={`http://ddragon.leagueoflegends.com/cdn/8.4.1/img/profileicon/${this.props.profileicon}.png`} />
+        <div className="sum-image">
+          <img className="card-image-top" style={{width: '100%'}} src={`http://ddragon.leagueoflegends.com/cdn/8.4.1/img/profileicon/${this.props.profileicon}.png`} />
+        </div>
         <div className="card-body">
           <h5 className="card-title">{this.props.name}</h5>
           <p className="card-text">Level: {this.props.level}</p>
