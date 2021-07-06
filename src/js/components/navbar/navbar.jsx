188fd03b93a68a86c72ceaf6ba44799cd4bc03ec
Abbas Soloki <solokiabbas@gmail.com>
2021-07-05 20:47:59 -0400

    fixed site

--- src/js/components/navbar/navbar.jsx
+++ src/js/components/navbar/navbar.jsx
@@ -8,34 +8,34 @@ class NavBar extends React.Component {
         if(typeof this.props.location === "object"){
         return(
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light nav-custom">
-            <Link className="navbar-brand nav-home-title" to="/"><img className="nav-home" src="../../../assets/images/w-icon.png" />League Watcher</Link>
-            <div className="nav-others">
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-light nav-custom">
+              <Link className="navbar-brand nav-home-title" to="/"><img className="nav-home" src="../../../assets/images/w-icon.png" />League Watcher</Link>
+              
+              <div className="nav-others">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
 
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <Link className={this.props.location.pathname === "/about" ? "navbar-brand nav-size glow" : "navbar-brand nav-size"} to="/about">About</Link>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                  <Link className={this.props.location.pathname.substring(0,6) === "/stats" ? "navbar-brand nav-size glow" : "navbar-brand nav-size" } to="/stats">Stats</Link>
 
-              <img className="nav-images" src="../../../assets/images/rune.png" />
-              <Link className={this.props.location.pathname.substring(0,6) === "/stats" ? "navbar-brand nav-size glow" : "navbar-brand nav-size" } to="/stats">Stats</Link>
+                  <img className="nav-images" src="../../../assets/images/rune.png" />
+                  <Link className={this.props.location.pathname === "/summoners" ? "navbar-brand nav-size glow" : "navbar-brand nav-size"} to="/summoners">Summoners</Link>
 
-              <img className="nav-images" src="../../../assets/images/rune.png" />
-              <Link className={this.props.location.pathname === "/summoners" ? "navbar-brand nav-size glow" : "navbar-brand nav-size"} to="/summoners">Summoners</Link>
+                  <img className="nav-images" src="../../../assets/images/rune.png" />
+                  <Link className={this.props.location.pathname === "/champions" ? "navbar-brand nav-size glow" : "navbar-brand nav-size"} to="/champions">Champions</Link>
 
-              <img className="nav-images" src="../../../assets/images/rune.png" />
-              <Link className={this.props.location.pathname === "/champions" ? "navbar-brand nav-size glow" : "navbar-brand nav-size"} to="/champions">Champions</Link>
+                  <img className="nav-images" src="../../../assets/images/rune.png" />
+                  <Link className={this.props.location.pathname === "/spells" ? "navbar-brand nav-size glow" : "navbar-brand nav-size"} to="/spells">Spells</Link>
 
-              <img className="nav-images" src="../../../assets/images/rune.png" />
-              <Link className={this.props.location.pathname === "/spells" ? "navbar-brand nav-size glow" : "navbar-brand nav-size"} to="/spells">Spells</Link>
+                  <img className="nav-images" src="../../../assets/images/rune.png" />
+                  <Link className={this.props.location.pathname === "/items" ? "navbar-brand nav-size glow" : "navbar-brand nav-size"} to="/items">Items</Link>
+                </div>
+              </div>
 
-              <img className="nav-images" src="../../../assets/images/rune.png" />
-              <Link className={this.props.location.pathname === "/items" ? "navbar-brand nav-size glow" : "navbar-brand nav-size"} to="/items">Items</Link>
-            </div>
-          </div>
-
-        </nav>
+          </nav>
+        </div>
       </div>
     );
   }else{
--- src/js/components/navbar/welcome.jsx
+++ src/js/components/navbar/welcome.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Welcome = () => (
-  <div className="homepage">
-    <h2 className="text-center forced-value">Welcome to League-Watcher Site!</h2>
-    <h2 className="text-center forced-value">Here you find out about champions, spells, and items currently on League of Legends!</h2>
-    <h2 className="text-center forced-value">Search for your favorite Champions and learn more about them!</h2>
-    <h2 className="text-center forced-value">Click on the Links above to find out more!</h2>
+  <div>
+    <div className="landscape">
+      <img className="welcome-images" src="../../../assets/images/Champion-List.jpg" />
+    </div>
+    <div className="homepage">
+      <h2 className="text-center forced-value">Welcome to League-Watcher Site! <br />
+      Here you find out about champions, spells, and items currently on League of Legends!<br />
+      Search for your favorite Champions and learn more about them!<br />
+      Click on the Links above to find out more!<br />
+      </h2>
+    </div>
   </div>
 );
 
